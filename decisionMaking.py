e321c2bb392c6d7422c20ad536fe7321724e25ea
yashuPython <yashitaagarwal1994@gmail.com>
2017-07-30 00:21:06 +0530

    verified

---
+++ decisionMaking.py
@@ -0,0 +1,10 @@
+var1 = 0
+if var1:
+   print "1 - Got a true expression value"
+   print var1
+
+var2 = 12
+if var2:
+   print "2 - Got a true expression value"
+   print var2
+print "Good bye!"
---
+++ loops1.py
@@ -0,0 +1,13 @@
+a=4
+while(a<10):
+print 'count is', a
+a = a + 1
+
+print 'tata'
+
+
+# Output:
+# File "loops1.py", line 3
+#     print 'count is', a
+#         ^
+# IndentationError: expected an indented block
